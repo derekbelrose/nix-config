eb6ba0c389efb7dcb259a69b2a8b7df158cfec3f
Derek Belrose <derek@derekbelrose.com>
2024-01-18 10:52:45 -0500

    Adding to my nix config

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "Derek's System Flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+	outputs = {self, nixpkgs, ... }: 
+	let 
+		lib = nixpkgs.lib;	
+	in {
+		nixosConfigurations = {
+			emeritus = lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+				];
+			};
+		};		
+	};
+}
