9cf95eaf72495c81bd25366c23d05ef3f17856b4
Derek Belrose <derek@derekbelrose.com>
2024-03-18 23:59:07 -0400

    troubleshooting

--- flake.nix
+++ flake.nix
@@ -6,28 +6,18 @@
 		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
 
-		nix-darwin.url = "github:lnl7/nix-darwin";
-		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
 		home-manager.url = "github:nix-community/home-manager/release-23.11";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-		disko.url = "github:nix-community/disko";
-		disko.inputs.nixpkgs.follows = "nixpkgs";
-
-		vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
 	outputs = inputs@{ self
-		, nixpkgs, nixpkgs-unstable, nixpkgs-darwin
-		, home-manager, nix-darwin, disko, vscode-server, nixos-hardware, ... }:
+		, nixpkgs, nixpkgs-unstable
+		, home-manager, nix-darwin, nixos-hardware, ... }:
   let
-		inputs = { inherit disko home-manager nixpkgs nixpkgs-unstable nix-darwin; };
+		inputs = { inherit home-manager nixpkgs nixpkgs-unstable nix-darwin; };
 
 		genPkgs = system: import nixpkgs { inherit system; config.allowUnfree = true; };
 		genUnstablePkgs = system: import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-		genDarwinPkgs = system: import nixpkgs-darwin { inherit system; config.allowUnfree = true; };
-
 		nixosSystem = system: hostname: username:
 			let
 				pkgs = genPkgs system;
--- hosts/nixos/framework/default.nix
+++ hosts/nixos/framework/default.nix
@@ -4,7 +4,6 @@
 	imports =
 		[
 			./hardware-configuration.nix
-			./../../common/common-packages.nix
 		];
 
 		boot.loader.systemd-boot.enable = true;
@@ -14,75 +13,71 @@
 
 		users.users.derek = { 		
 			isNormalUser = true;
-			extraGroups = [ "networkmanager" "wheel" "docker" "libvirt" "kvm" ];
+			extraGroups = [ "networkmanager" "wheel" ];
 			packages = with pkgs; [
-				bitwarden-cli
-				firefox
-				emacs
-				virt-manager
-				todoist-electron
-				slack
+				just
+				vim
 
 				unstablePkgs.vscode
 			];
 		};
 
-		systemd.services.systemd-udevd.restartIfChanged = false;
-		systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
-		systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
-
-		services.fwupd.enable = true;
-		services.tailscale.enable = true;
-		services.flatpak.enable = true;
-
-		sound.enable = true;
-		hardware.pulseaudio.enable = false;
-		security.rtkit.enable = true;
-		services.pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-		};
-
-		services.printing.enable = true;
-	
-		services.avahi = {
-			enable = true;
-			nssmdns = true;
-			openFirewall = true;
-		};
-
-		services.xserver.enable = true;
-
-		services.xserver.desktopManager.plasma6.enable = true;
-		services.xserver.displayManager.gdm.enable = true;
-		services.xserver.displayManager.gdm.wayland = true;
-
-		services.xserver.xkb = {
-			layout = "us";
-			variant = "";
-		};
-
-		virtualisation.libvirtd.enable = true;
-		virtualisation.spiceUSBRedirection.enable = true;
-		virtualisation = {
-			docker = {
-				enable = true;
-				autoPrunt = {
-					enable = true;
-					dates = "weekly";
-				};
-			};
-		};
-
-
-		programs.partition-manager.enable = true;
-		programs.kdeconnect.enable = true;
-
-		programs.steam = {
-			enable = true;
-		};
+#		systemd.services.systemd-udevd.restartIfChanged = false;
+#		systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+#		systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+#
+#		services.fwupd.enable = true;
+#		services.tailscale.enable = true;
+#		services.flatpak.enable = true;
+#
+#		sound.enable = true;
+#		hardware.pulseaudio.enable = false;
+#		security.rtkit.enable = true;
+#		services.pipewire = {
+#			enable = true;
+#			alsa.enable = true;
+#			alsa.support32Bit = true;
+#			pulse.enable = true;
+#		};
+#
+#		services.printing.enable = true;
+#	
+#		services.avahi = {
+#			enable = true;
+#			nssmdns = true;
+#			openFirewall = true;
+#		};
+#
+#		services.xserver.enable = true;
+#
+#		services.xserver.desktopManager.plasma6.enable = true;
+#		services.xserver.displayManager.gdm.enable = true;
+#		services.xserver.displayManager.gdm.wayland = true;
+#
+#		services.xserver.xkb = {
+#			layout = "us";
+#			variant = "";
+#		};
+#
+#		virtualisation.libvirtd.enable = true;
+#		virtualisation.spiceUSBRedirection.enable = true;
+#		virtualisation = {
+#			docker = {
+#				enable = true;
+#				autoPrunt = {
+#					enable = true;
+#					dates = "weekly";
+#				};
+#			};
+#		};
+#
+#
+#		programs.partition-manager.enable = true;
+#		programs.kdeconnect.enable = true;
+#
+#		programs.steam = {
+#			enable = true;
+#		};
 
 		system.stateVersion = "23.11";
 }
