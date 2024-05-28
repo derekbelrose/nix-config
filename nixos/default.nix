def27cd11a06d3774ad0ddd7a7850a7ba2827a06
Derek Belrose <derek@derekbelrose.com>
2024-05-27 23:10:40 -0400

    adding nix-commuity cachix

--- nixos/default.nix
+++ nixos/default.nix
@@ -132,10 +132,12 @@ in
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+				"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       substituters = [
         "https://cache.nixos.org"
         "https://nixpkgs-wayland.cachix.org"
+				"https://nix-community.cachix.org"
       ];
     };
   };
@@ -176,7 +178,6 @@ in
   networking.networkmanager.enable = true;
 
   networking.hostName = hostname;
-
 
   users.users.${username} = {
     isNormalUser = true;
