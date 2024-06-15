ba49576772381de3884fa596381ae0a1df75c09c
Derek Belrose <derek@derekbelrose.com>
2024-06-15 12:59:01 -0400

    removing network manager

--- nixos/gula/default.nix
+++ nixos/gula/default.nix
@@ -37,10 +37,8 @@
 
 
   networking.hostName = "gula"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.hostId = "2cbc7865";
+
 
 	services.smartd.enable = true;
   # Set your time zone.
