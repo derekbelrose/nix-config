f4d7853a4c80babcf030c2947a16f124f9e080fa
Derek Belrose <derek@derekbelrose.com>
2024-06-23 15:00:27 -0400

    Adding some SSH config and o thers

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware?ref=1e679b9";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
--- home-manager/_mixins/users/derek/default.nix
+++ home-manager/_mixins/users/derek/default.nix
@@ -8,27 +8,126 @@ in
     #../../services/syncthing.nix
   ];
 
+	nix = {
+		gc = {
+			automatic = true;
+			frequency = "15d";
+		};
+	};
+
+	pam.yubico.authorizedYubiKeys.ids = [
+		"cccccbbjhhhc"
+	];
+
 	home.packages = with pkgs; [
 	];
 
- programs = {
-    #fish.interactiveShellInit = ''
-    #  set -x GH_TOKEN (cat ${config.sops.secrets.gh_token.path})
-    #  set -x GITHUB_TOKEN (cat ${config.sops.secrets.gh_token.path})
-    #'';
-    git = {
-      userEmail = "derek@derekbelrose.com";
-      userName = "Derek Belrose";
-    };
-		emacs = {
-			enable = true;
-		};
-  };
-
-	home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ"; 
-	home.file.".config/alacritty/alacritty.toml".source =  ./sources/alacritty.toml;
-
 	
+	home.pointerCursor = {
+		package = pkgs.breeze-gtk;
+		name = "Breeze_Snow";
+		size = 24;
+	}; 
+
+	programs = {
+	   #fish.interactiveShellInit = ''
+	   #  set -x GH_TOKEN (cat ${config.sops.secrets.gh_token.path})
+	   #  set -x GITHUB_TOKEN (cat ${config.sops.secrets.gh_token.path})
+	   #'';
+	   git = {
+	     userEmail = "derek@derekbelrose.com";
+	     userName = "Derek Belrose";
+	   };
+	 	ssh = {
+	 		enable = true;
+	 		package = pkgs.openssh;
+	 		addKeysToAgent = "yes";
+	 		controlMaster = "yes";
+	 		controlPersist = "10m";
+	 		matchBlocks = {
+	 			"github.com" = {
+	 				user = "git";
+	 				identityFile = "~/.ssh/github";
+	 			};
+	 			"gula" = {
+	 				identityFile = "~/.ssh/gula";
+	 			};
+	 		};
+	 	};
+	 	emacs = {
+	 		enable = true;
+	 	};
+	 	firefox = {
+	 		enable = true;
+	 		package = pkgs.firefox-wayland;
+	 	};
+	 	alacritty = {
+	 		enable = true;
+	 		settings = {
+	 			cursor.style = {
+	 				shape = "beam";
+	 				blinking = "on";
+	 			};
+	 			font = {
+	 				size = 14;
+	 				normal = {
+	 					family = "FiraMono Nerd Font"; 
+	 					style = "Regular";
+	 				};
+	 				bold = {
+	 					family = "FiraMono Nerd Font";
+	 					style = "Bold";
+	 				};
+	 				italic = {
+	 					family = "FiraMono Nerd Font";
+	 					style = "Italic";
+	 				};
+	 			};
+	 			colors = {
+	 				primary = {
+	 					background = "0x2D0D16";
+	 					foreground = "0xf8f8f0";
+	 				};
+	 				normal = {
+	 					black = "0x464258";
+	 					red = "0xff857f";
+	 					green = "0xad5877";
+	 					yellow = "0xe6c000";
+	 					blue = "0x6c71c4";
+	 					magenta = "0xb267e6";
+	 					cyan = "0xafecad";
+	 					white = "0xcccccc";
+	 				};
+	 				bright = {
+	 					black = "0xc19fd8";
+	 					red = "0xf44747";
+	 					green = "0xffb8d1";
+	 					yellow = "0xffea00";
+	 					blue = "0x6796e6";
+	 					magenta = "0xc5a3ff";
+	 					cyan = "0xb2ffdd";
+	 					white = "0xf8f8f0";
+	 				};
+	 			};
+	 		};
+	 	};
+	 };
+
+	#home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ"; 
+	#home.file.".config/alacritty/alacritty.toml".source =  ./sources/alacritty.toml;
+
+
+	home.file.".icons/default".source = "${pkgs.gnome.adwaita-icon-theme}/share/icons/Adwaita";
+
+	gtk = {
+		enable = true;
+		iconTheme.name = "Papirus";
+		iconTheme.package = pkgs.papirus-icon-theme;
+		theme.name = "Yaru-dark";
+		theme.package = pkgs.yaru-theme;
+		cursorTheme.package = pkgs.breeze-gtk;
+		cursorTheme.name = "Breeze_Snow";
+	};
 
   #sops = {
   #  age = {
