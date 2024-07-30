4b793cb1364ff26e31ffe1558d7d8f18dc37e901
Derek Belrose <derek@derekbelrose.com>
2024-07-30 00:40:10 -0400

    Moving away from bcachefs, I'm just not ready to deal with this yet

--- nixos/gula/default.nix
+++ nixos/gula/default.nix
@@ -29,22 +29,23 @@
 
   boot.kernelModules = [ "kvm-intel" "zfs" "bcachefs" ];
   boot.kernelPackages = lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages;
-	boot.zfs.extraPools = [ ];
+	boot.zfs.extraPools = [ "store" ];
 
 	systemd.network.netdevs.eno1np0.enable = false;
 
-	systemd.services.mount-store = {
-		description = "Mount bcachefs /store";
-		script = "/run/current-system/sw/bin/mount -t bcachefs /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N0MAR4RJ:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N0M3REZC:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N6FT8CJC:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N4PP8377:/dev/disk/by-id/ata-CT1000P3SSD8_2307E6ABF4BC:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZGY6VHBS:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZM40Z1YD:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZM40ZH68:/dev/disk/by-id/ata-ST12000VN0008-2PH103_ZTN1CLT3:/dev/disk/by-id/ata-ST12000VN0008-2YS101_ZRT1FXD9 /store";
-		wantedBy = [ "multi-user.target" ];
-		enable = true;
-	};
+	#systemd.services.mount-store = {
+	#	description = "Mount bcachefs /store";
+	#	script = "/run/current-system/sw/bin/mount -t bcachefs /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N0MAR4RJ:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WMC4N0M3REZC:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N6FT8CJC:/dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N4PP8377:/dev/disk/by-id/ata-CT1000P3SSD8_2307E6ABF4BC:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZGY6VHBS:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZM40Z1YD:/dev/disk/by-id/ata-ST4000VN008-2DR166_ZM40ZH68:/dev/disk/by-id/ata-ST12000VN0008-2PH103_ZTN1CLT3:/dev/disk/by-id/ata-ST12000VN0008-2YS101_ZRT1FXD9 /store";
+	#	wantedBy = [ "multi-user.target" ];
+	#	enable = true;
+	#};
 
 
   networking.hostName = "gula"; # Define your hostname.
   networking.hostId = "2cbc7865";
 
 
+	services.zfs.autoScrub.enable = true;
 	services.systembus-notify.enable = true;
 	services.smartd = {
 		enable = true;
