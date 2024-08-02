5c2daf0e4b95c0ca5aae3db96a32aa8c3837e76b
Derek Belrose <derek@derekbelrose.com>
2024-08-01 21:31:59 -0400

    Removing gtk theme stuff, it doesn't belong in sway

--- modules/sway.nix
+++ modules/sway.nix
@@ -15,21 +15,20 @@ let
     '';
   };
 
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text =
-      let
-        schema = pkgs.gsettings-desktop-schemas;
-        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in
-      ''
-        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-        gnome_schema=org.gnome.desktop.interface
-        gsettings set $gnome_schema gtk-theme 'Dracula'
-      '';
-  };
+  #configure-gtk = pkgs.writeTextFile {
+  #  name = "configure-gtk";
+  #  destination = "/bin/configure-gtk";
+  #  executable = true;
+  #  text =
+  #    let
+  #      schema = pkgs.gsettings-desktop-schemas;
+  #      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+  #    in
+  #    ''
+  #      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+  #      gnome_schema=org.gnome.desktop.interface
+  #    '';
+  #};
 in
 {
   services.dbus.enable = true;
@@ -76,7 +75,6 @@ in
       htop
       pulseaudio
       dbus-sway-environment
-      configure-gtk
       jq
     ];
     extraSessionCommands = ''
